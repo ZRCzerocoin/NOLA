1cf2401284a16f93222290b359d7d90891cce63d
NOLA <ahmmohdaf@gmail.com>
2025-12-05 18:22:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // Set base to the repo path for GitHub Pages if you deploy there.
 // Update base to '/' if you deploy to root domain or you're not using GH pages.
 export default defineConfig({
-  base: '/NOLA/',
+  base: '/',
   server: {
     port: 5173
   }
