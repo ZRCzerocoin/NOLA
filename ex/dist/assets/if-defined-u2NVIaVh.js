1252abb80d0c6a8ad79982396433356687933f73
NOLA <ahmmohdaf@gmail.com>
2025-12-06 12:25:52 +0000

    diuyon

---
+++ ex/dist/assets/if-defined-u2NVIaVh.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CQyD2HdV.js";const m=o=>o??r;export{m as o};
---
+++ ex/src/main.js
@@ -0,0 +1,44 @@
+
+import { createAppKit } from "@reown/appkit";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+
+// 1. Get projectId from https://dashboard.reown.com
+const projectId = "9ae9b4de3d3181b8ea8fda287858f35e";
+
+// 2. Create your application's metadata object
+const metadata = {
+  name: "AppKit",
+  description: "AppKit Example",
+  url: "https://nol.pages.dev", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+// 3. Create a AppKit instance
+const modal = createAppKit({
+  adapters: [new EthersAdapter()],
+  networks: [mainnet, arbitrum],
+  metadata,
+  projectId,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+// Trigger modal programaticaly
+// Add this code inside `main.js` file at the end of the code file
+const openConnectModalBtn = document.getElementById("open-connect-modal");
+const openNetworkModalBtn = document.getElementById("open-network-modal");
+
+openConnectModalBtn.addEventListener("click", () => modal.open());
+openNetworkModalBtn.addEventListener("click", () =>
+  modal.open({ view: "Networks" })
+);
+import { BrowserProvider, Contract, parseEther } from "ethers";
+
+const provider = await modal.subscribeProviders((state) => {
+  return state["eip155"];
+});
+
+const addressFrom = await modal.subscribeAccount((state) => {
+  return state;
+});
