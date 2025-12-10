ea4f2387a5c609d2a9aeeb873310fe5f97af9629
NOLA <ahmmohdaf@gmail.com>
2025-12-10 16:01:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-// config.js
-window.BARRIER_KEY = "__BARRIER_KEY__";  // Cloudflare Pages injects real key
-window.COINGECKO = "__COINGECKO__";      // optional, not required by Coingecko open API
+// config.js (example)
+const SANBASE = "";
+const COINGECKO = "";
+window.SANBASE = SANBASE;
+window.COINGECKO = COINGECKO;
